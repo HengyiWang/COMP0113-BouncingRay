eda37e9388930fb9f7738588bd4374623612525b
ankus-wu <1498793919@qq.com>
2022-03-02 23:37:35 +0000

    add UI to show scores

---
+++ Assets/Scripts/UIScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScore : MonoBehaviour
+{
+    public Text ShowScore;
+    public int Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score = GameObject.Find("ScoreBox").GetComponent<ScoreManager>().score;
+        ShowScore.text = "Score: " + Score.ToString();
+    }
+}
