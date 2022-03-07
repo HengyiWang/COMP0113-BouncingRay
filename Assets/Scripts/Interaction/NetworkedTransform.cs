3242a6986999e6755eb17d90d080b2d447bfd804
LiuChen <strangejugg@hotmail.com>
2022-03-07 15:45:01 +0000

    add NetworkedTransform and test cube

---
+++ Assets/Scripts/Interaction/NetworkedTransform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ubiq.Messaging;
+
+public class NetworkedTransform : MonoBehaviour, INetworkComponent, INetworkObject
+{
+    public NetworkId Id { get; } = new NetworkId();
+    NetworkContext ctx;
+
+    struct SynchronizedTransform
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        // public Vector3 scale;
+    }
+
+    // private ownership;
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var msg = message.FromJson<SynchronizedTransform>();
+        transform.position = msg.position;
+        transform.rotation = msg.rotation;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ctx = NetworkScene.Register(this); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var grasped = GetComponent<MyFollowGraspable>().grasped;
+        if (grasped)
+        {
+            SynchronizedTransform message;
+            message.position = transform.position;
+            message.rotation = transform.rotation;
+            ctx.SendJson(message);
+        }
+    }
+}
