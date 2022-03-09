57dc81d53e310efdb32a2c55399b02ee741e7344
ankus-wu <1498793919@qq.com>
2022-03-09 00:44:06 +0000

    hit mirror effect

--- Assets/Scripts/Mechanism/ScoreMirror.cs
+++ Assets/Scripts/Mechanism/ScoreMirror.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ScoreMirror : MonoBehaviour
 {
     public bool hitted;
+    public Vector3 hitPoint = new Vector3(0,0,0);
+    private Vector3 lastHitPoint = new Vector3(0, 0, 0);
+    private float ftime;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +15,35 @@ public class ScoreMirror : MonoBehaviour
     }
 
     // Update is called once per frame
+    
     void Update()
     {
-        
+        SimpleSonarShader_Parent parent = GetComponentInParent<SimpleSonarShader_Parent>();
+        if (hitPoint != new Vector3(0,0,0))
+        {
+            if (hitPoint != lastHitPoint)
+            {
+                parent.StartSonarRing(hitPoint, 10.0f);
+            }
+            else
+            {
+                ftime += Time.deltaTime;
+                if (ftime >= 1f)
+                {
+                    parent.StartSonarRing(hitPoint, 10.0f);
+                    ftime = 0f;
+                }
+            }
+
+        }
+        //while (hitPoint != new Vector3(0, 0, 0))
+        //{
+        //    if(hitPoint != lastHitPoint)
+        //    {
+        //        parent.StartSonarRing(hitPoint, 10.0f);
+        //    }
+        //}
+        lastHitPoint = hitPoint;
+        hitPoint = new Vector3(0, 0, 0);
     }
 }
