5155bfb9f4bdc9783470dbbf940539b83b75b29b
LiuChen <strangejugg@hotmail.com>
2022-03-16 22:04:17 +0000

    activated Emissive

---
+++ Assets/Scripts/Mechanism/ActivatedEmissive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatedEmissive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxEmissive;
+    public ScoreMirror scoreMirrorComp;
+    public Material m;
+
+    private void Awake()
+    {
+        this.maxEmissive = 0.5f;
+        this.scoreMirrorComp = GetComponent<ScoreMirror>();
+        this.m = GetComponent<Renderer>().material;
+    }
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scoreMirrorComp && scoreMirrorComp.hitted)
+        {
+            if (m)
+            {
+                m.SetFloat("_Emission", maxEmissive * Mathf.Abs(Mathf.Sin(Time.time)));
+            }
+        }
+    }
+}
