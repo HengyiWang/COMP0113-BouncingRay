cb48ed2a71a37f99bfcdbb4598e409a011957e55
ankus-wu <1498793919@qq.com>
2022-03-16 22:25:13 +0000

    floating hand

---
+++ Assets/Scripts/Mechanism/Shaking.cs
@@ -0,0 +1,25 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Shaking : MonoBehaviour
+//{
+//    public float radian = 0;
+//    public float radianChangeRate = 0.02f;
+//    float radius = 0.8f;
+//    Vector3 oldRot;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        radian = Random.Range(0, Mathf.PI);
+//        oldRot = transform.rotation;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        radian += radianChangeRate;
+//        float dy = Mathf.Cos(radian) * radius;
+//        transform.rotation = oldRot + new Vector3(0, dy, 0);
+//    }
+//}
