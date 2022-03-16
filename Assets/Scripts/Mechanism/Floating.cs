c3a1efa8381f26915993be877e72a84fb372ce71
wanghy <wanghengyi@gmail.com>
2022-03-16 21:50:31 +0000

    Moving object level1

--- Assets/Scripts/Mechanism/Floating.cs
+++ Assets/Scripts/Mechanism/Floating.cs
@@ -6,7 +6,7 @@ public class Floating : MonoBehaviour
 {
     public float radian = 0; 
     public float radianChangeRate = 0.02f; 
-    float radius = 0.8f; 
+    public float radius = 0.8f; 
     Vector3 oldPos;
     // Start is called before the first frame update
     void Start()
