8037a19dfc106fac960656db042236fee9798786
ankus-wu <1498793919@qq.com>
2022-03-16 21:18:20 +0000

    fix floating

---
+++ Assets/Scripts/Mechanism/Floating.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    public float radian = 0; 
+    public float radianChangeRate = 0.02f; 
+    float radius = 0.8f; 
+    Vector3 oldPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        radian = Random.Range(0, Mathf.PI);
+        oldPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        radian += radianChangeRate; 
+        float dy = Mathf.Cos(radian) * radius; 
+        transform.position = oldPos + new Vector3(0, dy, 0);
+    }
+}
