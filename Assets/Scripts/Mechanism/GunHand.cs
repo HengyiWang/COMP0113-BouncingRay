9d0088f2f99c24b3a6f83043c858fb09a7934c18
ankus-wu <1498793919@qq.com>
2022-03-17 14:48:33 +0000

    Hand guide and vanish when finish grasping task

---
+++ Assets/Scripts/Mechanism/GunHand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunHand : MonoBehaviour
+{
+    private GameObject all_laser_gun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Grap any gun destroy the hand.
+        all_laser_gun = GameObject.Find("All_Laser_Gun");
+        foreach (Transform child in all_laser_gun.transform)
+        {
+            if (child.GetComponent<GunGraspable>().grasped)
+            {
+                GameObject.Destroy(gameObject);
+            }
+        }
+    }
+}
