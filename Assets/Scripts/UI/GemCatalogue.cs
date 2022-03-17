1656cf3ded8f1f9c5ab5a3437276b0004e6a1cd7
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-03-17 01:49:47 +0000

    random generation level1

---
+++ Assets/Scripts/UI/GemCatalogue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gem Catalogue")]
+public class GemCatalogue : ScriptableObject
+{
+    public List<GameObject> gemPrefabs;
+
+    public int size { get { return gemPrefabs.Count; } }
+    public GameObject GetPrefab(int i)
+    {
+        return gemPrefabs[i];
+    }
+
+    
+}
