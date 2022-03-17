caf2687a4a962cfb9fdefc323e79f491219f36e0
wanghy <wanghengyi@gmail.com>
2022-03-17 16:13:07 +0000

    Fix the noise of the robot

--- Assets/Close_robot_behav.cs
+++ Assets/Close_robot_behav.cs
@@ -9,14 +9,14 @@ public class Close_robot_behav : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        sound = GetComponents<AudioSource>();
+        sound = animator.GetComponents<AudioSource>();
         sound[1].Play();
     }
 
-    private T[] GetComponents<T>()
-    {
-        throw new NotImplementedException();
-    }
+    //private T[] GetComponents<T>()
+    //{
+    //    throw new NotImplementedException();
+    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
