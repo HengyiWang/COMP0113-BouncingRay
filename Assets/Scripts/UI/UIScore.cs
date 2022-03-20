f8af621a87cf7e884d21ad4f6a42113e77c42c09
ankus-wu <1498793919@qq.com>
2022-03-20 19:03:11 +0000

    Scene of wch

--- Assets/Scripts/UI/UIScore.cs
+++ Assets/Scripts/UI/UIScore.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class UIScore : MonoBehaviour
 {
-    public Text ShowScore;
+    //public Text ShowScore;
     public int Score;
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,6 @@ public class UIScore : MonoBehaviour
     void Update()
     {
         Score = GameObject.Find("ScoreBox").GetComponent<ScoreManager>().score;
-        ShowScore.text = "Score: " + Score.ToString();
+        GetComponent<TextMesh>().text = "Score: " + Score.ToString();
     }
 }
