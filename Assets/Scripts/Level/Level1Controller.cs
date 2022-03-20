8bc47cf256e25672004748d85abd717a3974c512
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-03-20 21:48:18 +0000

    add controller level 1

---
+++ Assets/Scripts/Level/Level1Controller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Controller : LevelController
+{
+    public List<LevelEvent> endGameEvents;
+
+    protected override void SetUp()
+    {
+        initialize(endGameEvents);
+    }
+}
