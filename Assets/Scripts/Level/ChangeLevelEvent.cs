2c1c4ab336c58150d0e89800b90f3da72bd71968
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-03-20 20:44:51 +0000

    sync

--- Assets/Scripts/Level/ChangeLevelEvent.cs
+++ Assets/Scripts/Level/ChangeLevelEvent.cs
@@ -8,12 +8,14 @@ public class ChangeLevelEvent : LevelEvent
 
     public GameObject teleportor;
     public Vector3 position;
+    public string targetScene;
 
     private GameObject teleportorInstance;
 
     protected override void EventStart(ReadOnlyCollection<LevelEvent> previousEvents, Queue<LevelEvent> followingEvents)
     {
         teleportorInstance = Instantiate(teleportor, position, Quaternion.identity);
+        teleportorInstance.GetComponent<SceneSwitcher>().targetScene = targetScene;
     }
 
     protected override void EventUpdate(ReadOnlyCollection<LevelEvent> previousEvents, Queue<LevelEvent> followingEvents)
