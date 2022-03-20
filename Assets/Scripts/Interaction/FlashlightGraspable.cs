8189de19f52dd164740fa561b75ca69bfc639166
LiuChen <strangejugg@hotmail.com>
2022-03-20 17:16:11 +0000

    flahslight sync implemented

--- Assets/Scripts/Interaction/FlashlightGraspable.cs
+++ Assets/Scripts/Interaction/FlashlightGraspable.cs
@@ -3,35 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Ubiq.XR;
 
-public class FlashlightGraspable : MyFollowGraspable, IGraspable
+public class FlashlightGraspable : GunGraspable
 {
     
-    private Rigidbody rb;
-    // Start is called before the first frame update
-    public new void Grasp(Hand controller)
-    {
-        base.Grasp(controller);
-
-        rb.useGravity = false;
-        rb.isKinematic = true;
-    }
-
-    public new void Release(Hand controller)
-    {
-        base.Release(controller);
-        rb.useGravity = true;
-        rb.isKinematic = false;
-    }
-
-    public void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-        if (!rb)
-        {
-            Debug.LogError("No rigidbody in the flashlight!");
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/Mechanism/ShootLighting.cs
+++ Assets/Scripts/Mechanism/ShootLighting.cs
@@ -7,16 +7,25 @@ public class ShootLighting : MonoBehaviour
     // Start is called before the first frame update
     public bool clicked = false;
     private GameObject spotLight;
+    private NetworkedOwnership ownershipComp;
     void Start()
     {
+        ownershipComp = GetComponent<NetworkedOwnership>();
+        if (!ownershipComp)
+        {
+            Debug.LogError("ownership component is missing!");
+        }
         spotLight = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ownershipComp && ownershipComp.ownership)
+        {
+            clicked = Input.GetButton("Fire1");
+        }
         bool grasped = GetComponent<MyFollowGraspable>().grasped;
-        clicked = Input.GetButton("Fire1");
         if (grasped && clicked)
         {
             spotLight.SetActive(true);
