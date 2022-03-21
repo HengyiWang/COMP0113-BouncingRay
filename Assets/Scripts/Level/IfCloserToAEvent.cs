c2bfdc710bff81931ee3e6e554a4122e65dadbf0
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-03-21 00:42:45 +0000

    finish level0 guide

---
+++ Assets/Scripts/Level/IfCloserToAEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class IfCloserToAEvent : IfEvent
+{
+    public GameObject player;
+    public GameObject A;
+    public GameObject B;
+
+    public List<LevelEvent> trueEvents;
+    public List<LevelEvent> falseEvents;
+
+    protected override List<LevelEvent> caseFalseEvents()
+    {
+        return falseEvents;
+    }
+
+    protected override List<LevelEvent> caseTrueEvents()
+    {
+        return trueEvents;
+    }
+
+    protected override bool condition()
+    {
+        Vector3 playerPosition = player.transform.position;
+        return Vector3.Distance(playerPosition, A.transform.position) < Vector3.Distance(playerPosition, B.transform.position);
+    }
+}
---
+++ Assets/Scripts/Level/MirrorHandHintEvent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class MirrorHandHintEvent : LevelEvent
+{
+    public GameObject hand;
+    public Transform handTransform;
+
+    private GameObject instance;
+
+    protected override void EventStart(ReadOnlyCollection<LevelEvent> previousEvents, Queue<LevelEvent> followingEvents)
+    {
+        instance = Instantiate(hand, handTransform);
+    }
+
+    protected override void EventUpdate(ReadOnlyCollection<LevelEvent> previousEvents, Queue<LevelEvent> followingEvents)
+    {
+        
+    }
+
+    protected override bool IsCompleted()
+    {
+        foreach (Transform child in GameObject.Find("All_Gem").transform)
+        {
+            if (child.GetComponent<MirrorGraspable>().grasped)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public override void OnEventDestory()
+    {
+        Destroy(instance);
+    }
+}
