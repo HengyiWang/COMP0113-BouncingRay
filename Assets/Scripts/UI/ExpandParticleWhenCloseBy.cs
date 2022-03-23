595f87c94f272612ade7fa349d0b7b124c729349
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-03-23 11:32:09 +0000

    switch_v2 blackhole uses too much RAM fix

---
+++ Assets/Scripts/UI/ExpandParticleWhenCloseBy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandParticleWhenCloseBy : MonoBehaviour
+{
+
+    public float closeByDistance = 30f;
+    public GameObject optionalPlayer;
+
+    void Start()
+    {
+        GetComponent<ParticleSystem>().Stop();
+        if (optionalPlayer == null)
+        {
+            optionalPlayer = GameObject.Find("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+        if (distanceToPlayer() < closeByDistance)
+        {
+            if (particleSystem.isStopped)
+            {
+                particleSystem.Play();
+            }
+        }
+        else if(particleSystem.isPlaying)
+        {
+            particleSystem.Stop();
+        }
+
+    }
+
+    float distanceToPlayer()
+    {
+        return Vector3.Distance(optionalPlayer.transform.position, transform.position);
+    }
+}
