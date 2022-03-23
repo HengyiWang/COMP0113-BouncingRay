6762dbe6c28c1576d667047015fa65a71623a120
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-03-23 15:47:25 +0000

    remove unnecessary comments

--- Assets/Scripts/Mechanism/ScoreMirror.cs
+++ Assets/Scripts/Mechanism/ScoreMirror.cs
@@ -8,46 +8,6 @@ public class ScoreMirror : MonoBehaviour
     public bool played;
     public int colorID;
     public Vector3 hitPoint = new Vector3(0,0,0);
-    private Vector3 lastHitPoint = new Vector3(0, 0, 0);
-    private float ftime;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    
-    void Update()
-    {
-        //Debug.Log("Has been here");
-        // Sonar Shader Effect
-        //SimpleSonarShader_Parent parent = GetComponentInParent<SimpleSonarShader_Parent>();
-        //if (parent)
-        //{
-        //    if (hitPoint != new Vector3(0, 0, 0))
-        //    {
-        //        if (hitPoint != lastHitPoint)
-        //        {
-        //            parent.StartSonarRing(hitPoint, 10.0f);
-        //        }
-        //        else
-        //        {
-        //            ftime += Time.deltaTime;
-        //            if (ftime >= 1f)
-        //            {
-        //                parent.StartSonarRing(hitPoint, 10.0f);
-        //                ftime = 0f;
-        //            }
-        //        }
-
-        //    }
-        //}
-        
-        //lastHitPoint = hitPoint;
-        //hitPoint = new Vector3(0, 0, 0);
-        
-    }
 
     public void playAudioSource()
     {
